e5d4562e51973b5169188ad1e18ab0e4b0067141
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-06-20 09:14:05 +0530

    Initial FastAPI practice commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+    return data    
+
+
+@app.get("/")
+def hello():
+    return{'message': 'patient management system API'}
+
+@app.get("/about")
+def about():
+    return{'meassage': 'A fully functional API to manage patient records '}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+
