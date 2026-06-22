6f572aac779b2f3655db4f4d8da5cc61c5e83f9b
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-06-22 20:30:38 +0530

    field validator in pydantic

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 ### EmailStr, AnyUrl this are custome datatypes...
 ### Field(not custome) is useful for numerical and string based datatypes for custome data validation
 from typing import List , Dict , Optional , Annotated
@@ -10,11 +10,29 @@ class patient(BaseModel): ## BYDEFAULT ALL FIELDS ARE REQUIRED BUT CAN BE OPTION
     age: int = Field(gt=0 ,lt=120)
     ### COMPLEX TYPE VALIDATION
     weight: Annotated[float, Field(gt=0, strict=True)]
-    married: Annotated[bool, Field(default=None, description='is the patient married or not')]
+    married: Optional[bool] = None
     allergies: Annotated[Optional[list[str]], Field(default=None, max_length=5)] 
         #values in allergies list should be string 
         #there is optional there for if there in no allergies then it will show None          
     contect_details: Dict[str,str] #values in contect detail dictionary should be string and string
+
+    ### FIELD VALIDATOR (SHOULD BE INSIDE CLASS)
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domains = ['hdfc.com','icici.com']
+        ## abc@gmail.com
+        domain_name = value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError('not a valid domain')
+        return value
+
 
 patient_info = {'name':'om', 'age':20, 'weight':65.0, 'married':True, 'allergies':['pollen','dust'], 'contect_details':{'email':'abc@gamil.com','phone':'9876543210'}}
 
