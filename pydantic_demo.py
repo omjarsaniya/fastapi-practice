bbc9ccdf0202c0b62f425debc9b1f6936d01b994
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-06-22 20:58:44 +0530

    model validator in pydantic

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator
 ### EmailStr, AnyUrl this are custome datatypes...
 ### Field(not custome) is useful for numerical and string based datatypes for custome data validation
 from typing import List , Dict , Optional , Annotated
@@ -8,13 +8,14 @@ class patient(BaseModel): ## BYDEFAULT ALL FIELDS ARE REQUIRED BUT CAN BE OPTION
     ### TYPE VALIDATION
     name: Annotated[str, Field(max_length=50, title='name of the patient', description='give the name of the patient in less than 50 chars', examples=['om','foram'])]
     age: int = Field(gt=0 ,lt=120)
+    email: EmailStr
     ### COMPLEX TYPE VALIDATION
     weight: Annotated[float, Field(gt=0, strict=True)]
     married: Optional[bool] = None
     allergies: Annotated[Optional[list[str]], Field(default=None, max_length=5)] 
         #values in allergies list should be string 
         #there is optional there for if there in no allergies then it will show None          
-    contect_details: Dict[str,str] #values in contect detail dictionary should be string and string
+    contact_details: Dict[str,str] #values in contect detail dictionary should be string and string
 
     ### FIELD VALIDATOR (SHOULD BE INSIDE CLASS)
     @field_validator('name')
@@ -32,9 +33,17 @@ class patient(BaseModel): ## BYDEFAULT ALL FIELDS ARE REQUIRED BUT CAN BE OPTION
         if domain_name not in valid_domains:
             raise ValueError('not a valid domain')
         return value
+    
+    ### MODEL VALIDATOR
+    @model_validator(mode='after') ## DEFAULT MODE WILL BE AFTER (and it should be...)
+    def validate_emergency_contact(self):
+        if self.age > 60 and 'emergency' not in self.contact_details:
+            raise ValueError('patients older than 60 must have an emergency contact in contact details')
+        return self
 
 
-patient_info = {'name':'om', 'age':20, 'weight':65.0, 'married':True, 'allergies':['pollen','dust'], 'contect_details':{'email':'abc@gamil.com','phone':'9876543210'}}
+
+patient_info = {'name':'om', 'age':65, 'weight':65.0, 'email':'abc@hdfc.com', 'married':True, 'allergies':['pollen','dust'], 'contact_details':{'email':'abc@gamil.com','phone':'9876543210','emergency':'565656'}}
 
 ### INSERTING 
 def insert_patient_data(patient: patient):
@@ -54,3 +63,4 @@ def update_patient_data(patient: patient):
     print('updated patient data')
 
 update_patient_data(patient1)
+
