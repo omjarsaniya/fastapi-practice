72dfe5ae4241fb4b524c0142431a8e7fd28da168
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-06-22 14:00:24 +0530

    complex type validation in pydantic

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel
+from typing import List , Dict
 
 class patient(BaseModel):
 
     ### TYPE VALIDATION
     name: str
     age: int
+    ### COMPLEX TYPE VALIDATION
+    weight: float
+    married: bool
+    allergies: List[str] #values in allergies list should be string
+    contect_details: Dict[str,str] #values in contect detail dictionary should be string and string
 
-patient_info = {'name':'om' , 'age':20}
+patient_info = {'name':'om', 'age':20, 'weight':65, 'married':True, 'allergies':['pollen','dust'], 'contect_details':{'email':'abc@gamil.com','phone':'9876543210'}}
 
 ### INSERTING 
 def insert_patient_data(patient: patient):
